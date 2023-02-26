2b6b8b7ece7b10e8bf21bed39b7e1e455524fc5b
Marzouk OG <marzoukog@outlook.be>
2023-02-26 09:56:41 +0100

    markov model done (needs testing)

--- markov_model.cpp
+++ markov_model.cpp
@@ -2,41 +2,41 @@
 #include <error.h>
 #include "markov_model.hpp"
 
-
-void markov_model(Markov_model& m, unsigned int order, const std::string& s){
-    //String must be longer than the order itself 
-    if(s.length() < order){
+void markov_model(Markov_model &m, unsigned int order, const std::string &s)
+{
+    // String must be longer than the order itself
+    if (s.length() < order)
+    {
         throw;
     }
 
-    for(int i = 0; i< s.length(); i++){
+    for (int i = 0; i < s.length(); i++)
+    {
         m.alphabet.insert(s[i]);
     }
 
     m.order = order;
 
     int occ = 0;
-    for(int i= 0; i+order<s.length(); i++){
-        
-            std::string tmp = s.substr(i, i+order);
-            if(m.model.find(tmp)->second)
-                occ = m.model.find(tmp)->second;
-            else
-               occ = 1;
-        
-            m.model[tmp] = occ;
+    for (int i = 0; i < s.length(); i++)
+    {
+        std::string tmp;
+        if (i + order < s.length())
+        {
+            tmp = s.substr(i, i + order);
+        }
+        else
+        {
+            for (int j = 0; j < order; j++)
+            {
+                tmp = tmp + s[(i + j) % s.length()];
+            }
+        }
+
+        if (m.model.find(tmp)->second)
+            occ = m.model.find(tmp)->second;
+        else
+            occ = 1;
+
+        m.model[tmp] = occ;
     }
-    
-}
-
-
-static std::string *generate_substrings()
-
-
-
-// static void fill_map(Model& mod,unsigned int order, const std::string& s){
-
-
-
-// }
-
