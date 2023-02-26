df5d35f06cd4d2d2b9a8949fa68b3c2b326d96bb
Marzouk OG <marzoukog@outlook.be>
2023-02-26 08:59:58 +0100

    working on markov model

--- markov_model.cpp
+++ markov_model.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
-
+#include <error.h>
 #include "markov_model.hpp"
 
+
+void markov_model(Markov_model& m, unsigned int order, const std::string& s){
+    //String must be longer than the order itself 
+    if(s.length() < order){
+        throw;
+    }
+
+    for(int i = 0; i< s.length(); i++){
+        m.alphabet.insert(s[i]);
+    }
+
+    m.order = order;
+
+    int occ = 0;
+    for(int i= 0; i+order<s.length(); i++){
+        
+            std::string tmp = s.substr(i, i+order);
+            if(m.model.find(tmp)->second)
+                occ = m.model.find(tmp)->second;
+            else
+               occ = 1;
+        
+            m.model[tmp] = occ;
+    }
+    
+}
+
+
+static std::string *generate_substrings()
+
+
+
+// static void fill_map(Model& mod,unsigned int order, const std::string& s){
+
+
+
+// }
 
