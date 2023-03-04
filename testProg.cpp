ad1d550a45345d461a9d1c13fa4488f3a942b868
Marzouk OG <marzoukog@outlook.be>
2023-03-04 16:33:11 +0100

    File

--- testProg.cpp
+++ testProg.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
+#include <fstream>
 #include "markov_model.hpp"
 using namespace std;
 
-int main(int argc, char** argv){
+typedef std::map<std::string, string> trainers;
+
+int main(int argc, char **argv)
+{
 
     Markov_model m;
 
-    markov_model(m, stoi(argv[0]), )
+    for (int i = 1; i < argc; i++)
+    {
+        string s(argv[i]);
+        if (s.substr(0, 2) != "TD")
+            break;
+        ifstream myfile;
+        myfile.open(s);
+    }
+
+    // markov_model(m, stoi(argv[0]), ):
 
     cout << "You have entered " << argc
-         << " arguments:" << "\n";
-  
+         << " arguments:"
+         << "\n";
+
     for (int i = 0; i < argc; ++i)
         cout << argv[i] << "\n";
-  
+
     return 0;
 }
