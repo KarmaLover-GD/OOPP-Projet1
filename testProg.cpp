f1867cfc58ff549953b96545a2dae406ee0d31fa
Marzouk OG <marzoukog@outlook.be>
2023-03-04 19:52:54 +0100

    Loading args complete

--- testProg.cpp
+++ testProg.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include "markov_model.hpp"
 using namespace std;
 
@@ -7,20 +8,38 @@ typedef std::map<std::string, string> trainers;
 
 int main(int argc, char **argv)
 {
+    vector<string> sample;
 
     Markov_model m;
-
+    string tmp1, tmp2;
+    trainers tr;
     for (int i = 1; i < argc; i++)
     {
         string s(argv[i]);
-        if (s.substr(0, 2) != "TD")
-            break;
-        ifstream myfile;
-        myfile.open(s);
-        if (myfile.is_open()){
-            myfile.read();
-        }
+        if (s.substr(0, 2) == "TD")
+        {
+            ifstream myfile;
+            myfile.open(s);
+            if (myfile.is_open())
+            {
+                getline(myfile, tmp1);
+                getline(myfile, tmp2);
 
+                tr[tmp1] = tmp2;
+            }
+        }
+        else if (s.substr(0, 1) == "-")
+        {
+            continue;
+        }else if(s.substr(0, 1)=="I" ){
+            ifstream myfile;
+            myfile.open(s);
+            if (myfile.is_open())
+            {
+                getline(myfile, tmp1);
+                sample.push_back(tmp1);
+            }
+        }
     }
 
     // markov_model(m, stoi(argv[0]), ):
