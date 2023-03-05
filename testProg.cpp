922edd65b0ba6ffd62b5b8257d80b1c0255e9cbb
Marzouk OG <marzoukog@outlook.be>
2023-03-05 22:22:45 +0100

    working properly -> issues with printing exceptions

--- testProg.cpp
+++ testProg.cpp
@@ -5,40 +5,43 @@
 using namespace std;
 
 typedef std::map<std::string, string> trainers;
-static bool isNumber(const string& s)
+static bool isNumber(const string &s)
 {
-    for (char const &ch : s) {
-        if (std::isdigit(ch) == 0) 
+    for (char const &ch : s)
+    {
+        if (std::isdigit(ch) == 0)
             return false;
     }
     return true;
- }
+}
 int main(int argc, char **argv)
- {
-    string order(argv[0]);
-     if(!isNumber(order))
+{
+
+    string order(argv[1]);
+    if (!isNumber(order))
         throw domain_error("first argument should be a number");
-   
+
     vector<string> sample;
     Markov_model m;
     string tmp1, tmp2;
     trainers tr;
-    int sep_idx = 0; 
-    for (int i = 1; i < argc; i++)
+    int sep_idx = 0;
+    for (int i = 2; i < argc; i++)
     {
         string s(argv[i]);
-        if(s == "--")
-            sep_idx == i;
+        if (s == "--")
+            sep_idx = i;
     }
 
-    if(sep_idx == 0 || sep_idx > argc)
+    if (sep_idx == 0)
         throw domain_error("No separator found");
-    // Loading arguments and file contents 
-    for(int i =1; i<argc; i++){
+    // Loading arguments and file contents
+    for (int i = 2; i < argc; i++)
+    {
         string s(argv[i]);
-        if(i<sep_idx){
-             
-            
+        if (i < sep_idx)
+        {
+
             ifstream myfile;
             myfile.open(s);
             if (myfile.is_open())
@@ -47,7 +50,9 @@ int main(int argc, char **argv)
                 getline(myfile, tmp2);
                 tr[tmp1] = tmp2;
             }
-        }else if( i> sep_idx){
+        }
+        else if (i > sep_idx)
+        {
             ifstream myfile;
             myfile.open(s);
             if (myfile.is_open())
@@ -60,43 +65,37 @@ int main(int argc, char **argv)
     int value = -99999;
     int best = -99999;
     int best_idx = 0;
+    string best_name;
     auto it = tr.begin();
 
-    while(it != tr.end()){
+    while (it != tr.end())
+    {
         Markov_model m;
-        markov_model(m, stoi(argv[0]), it->second);
-        for(int i = 0; i<sample.size(); i++){
+        markov_model(m, stoi(argv[1]), it->second);
+        for (int i = 0; i < sample.size(); i++)
+        {
             try
             {
                 value = likelihood(m, sample[i]);
-                if(value>best){
+                if (value > best)
+                {
                     best = value;
                     best_idx = i;
                 }
             }
-            catch(const std::exception& e)
+            catch (const std::exception &e)
             {
                 std::cerr << e.what() << '\n';
-                cout<<"-";
+                cout << "-";
                 value = -9999999;
             }
-            cout<<value <<"is the likelihood for test"<<i <<"under trained set "<<it->first<<endl;
+            cout << value << " is the likelihood for test" << i << " under trained set " << it->first << endl;
         }
-        cout<<"the was with the highest likelihood was the number :"<<best_idx<< " Witrh a likelihood of"<<best<<endl;
+        cout << "the test with the highest likelihood for training set " <<it->first<< " was the number :" << best_idx << " With a likelihood of" << best << endl;
         best = -99999;
         best_idx = 0;
-
+        ++it;
     }
-    
-
-    // markov_model(m, stoi(argv[0]), ):
-
-    cout << "You have entered " << argc
-         << " arguments:"
-         << "\n";
-
-    for (int i = 0; i < argc; ++i)
-        cout << argv[i] << "\n";
 
     return 0;
- }
+}
