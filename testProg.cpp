c45a84169f38a91e9551d2af75b025f6b1689085
Marzouk <marzouk.ouro-gomma@student.uliege.be>
2023-03-05 07:45:41 +0100

    test prog file loading done

--- testProg.cpp
+++ testProg.cpp
@@ -5,33 +5,50 @@
 using namespace std;
 
 typedef std::map<std::string, string> trainers;
-
-int main(int argc, char **argv)
+static bool isNumber(const string& s)
 {
+    for (char const &ch : s) {
+        if (std::isdigit(ch) == 0) 
+            return false;
+    }
+    return true;
+ }
+int main(int argc, char **argv)
+ {
+    string order(argv[0]);
+     if(!isNumber(order))
+        throw domain_error("first argument should be a number");
+   
     vector<string> sample;
-
     Markov_model m;
     string tmp1, tmp2;
     trainers tr;
+    int sep_idx = 0; 
+
     for (int i = 1; i < argc; i++)
     {
         string s(argv[i]);
-        if (s.substr(0, 2) == "TD")
-        {
+        if(s == "--")
+            sep_idx == i;
+    }
+
+    if(sep_idx == 0 || sep_idx > argc)
+        throw domain_error("No separator found");
+    
+    for(int i =1; i<argc; i++){
+        string s(argv[i]);
+        if(i<sep_idx){
+             
+            
             ifstream myfile;
             myfile.open(s);
             if (myfile.is_open())
             {
                 getline(myfile, tmp1);
                 getline(myfile, tmp2);
-
                 tr[tmp1] = tmp2;
             }
-        }
-        else if (s.substr(0, 1) == "-")
-        {
-            continue;
-        }else if(s.substr(0, 1)=="I" ){
+        }else if( i> sep_idx){
             ifstream myfile;
             myfile.open(s);
             if (myfile.is_open())
@@ -41,6 +58,14 @@ int main(int argc, char **argv)
             }
         }
     }
+       
+        // else if (s.substr(0, 1) == "-")
+        // {
+        //     continue;
+        // }else if(s.substr(0, 1)=="I" ){
+            
+        // }
+    
 
     // markov_model(m, stoi(argv[0]), ):
 
@@ -52,4 +77,4 @@ int main(int argc, char **argv)
         cout << argv[i] << "\n";
 
     return 0;
-}
+ }
